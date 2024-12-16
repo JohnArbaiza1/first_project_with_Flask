cce48a57b348a2ed3d2d7066867c57ee92772eb6
JohnArbaiza1 <johnarbaiza07.03@gmail.com>
2024-12-15 21:09:30 -0600

    Se agregan algunas cosas

--- App/myApp.py
+++ App/myApp.py
@@ -5,16 +5,24 @@ from flask import render_template
 #Definimos una variable para asignar la instancia de la clase
 app = Flask(__name__)
 
-#utilizamos el decorador route() para indicar a Flask qué URL debe activar nuestra función.
+#utilizamos el decorador route() para indicar a Flask qué URL debe activar nuestra función
 @app.route('/')
 #Defiimos una funcion para que retornen nuestro template
 def inicio():
     #Diccionario de dato que pasamos al layout 
     datos = {
-        'titulo': 'First project with Flask ',
+        'titulo': 'First project with Flask',
         'Mensaje': 'Hola Mundo'
     }
-    return render_template('/Views/index.html', data = datos)
+    return render_template('/Views/index.html', data=datos)
+
+@app.route('/Home')
+def home_redirect():
+    return redirect(url_for('inicio'))
+
+@app.route("/info")
+def info():
+      return render_template('/Views/info.html');
 
 #Comprobamos el nombre de la aplicación
 if __name__ == '__main__':
